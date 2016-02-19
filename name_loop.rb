4a3fac7a4ad66db2430e1ace4cbe8e404d5bf2ba
sophie06rae <sophie.rae.06@gmail.com>
2016-02-19 14:02:24 +0000

    Create name_loop.rb

---
+++ name_loop.rb
@@ -0,0 +1,18 @@
+
+def get_name 
+  name = ''
+  
+  loop do
+    print "Please enter your name. Name must be longer than two characters and containe no numbers:"
+    name = gets.chomp 
+    if name.length >2 && !name.index(/\d/)
+      break 
+      else 
+      print "error please re enter your name"
+   end 
+   end 
+    return name
+   end 
+ 
+  name = get_name()
+  puts "Hi #{name}" 
